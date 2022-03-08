e6bd38bc6439290c0796de190ac300bc4a4e57d8
Familia <Familia@Famlia>
2022-03-08 15:52:44 -0300

    Envio de novas classes

--- HospedagemPets/src/Cliente.java
+++ HospedagemPets/src/Cliente.java
@@ -1,5 +1,22 @@
+import java.util.Date;
 
 public class Cliente extends Usuario {
 	private String cidadeCliente;
-	private Servico servico;
+	private Pet pet;
+
+	public String getCidade() {
+		return cidadeCliente;
+	}
+	
+	public void setCidade(String cidadeCliente) {
+		this.cidadeCliente = cidadeCliente;
+	}
+	
+	public Pet getPet() {
+		return pet;
+	}
+	
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
 }
---
+++ HospedagemPets/src/Visita.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+public class Visita extends Servico {
+	private Date dataVisita;
+	private int horaVisita;
+	private String enderecoCliente;
+	
+	public Date getDataVisita() {
+		return dataVisita;
+	}
+	
+	public void setDataVisita(Date dataVisita) {
+		this.dataVisita = dataVisita;
+	}
+	
+	public int getHoraVisita() {
+		return horaVisita;
+	}
+	
+	public void setHoraVisita(int horaVisita) {
+		this.horaVisita = horaVisita;
+	}
+	
+	public String getEndereco() {
+		return enderecoCliente;
+	}
+	
+	public void setEndereco(String enderecoCliente) {
+		this.enderecoCliente = enderecoCliente;
+	}
+}
